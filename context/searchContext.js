92c05587a7fa0a2931681544594ab3acc32e23be
Leoasa1 <leoasa84@gmail.com>
2022-11-10 00:21:08 -0800

    Added navbar and pages

---
+++ context/searchContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const searchContext = () => {
+	return <div>searchContext</div>;
+};
+
+export default searchContext;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '@tomtom-international/web-sdk-maps/dist/maps.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
