be8ee5db1f2cc9578be86d6c86a786dac22be910
Leoasa1 <leoasa84@gmail.com>
2022-11-11 20:41:13 -0800

    Added components

---
+++ components/layout/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../navbar/Navbar';
+
+const Layout = (title, description, keywords, children) => {
+	return (
+		<div data-theme='bumblebee'>
+			<Head>
+				<title>{title}</title>
+				<meta name='description' content={description} />
+				<meta name='keywords' content={keywords} />
+			</Head>
+			<Navbar />
+			{children}
+		</div>
+	);
+};
+
+export default Layout;
+
+Layout.defaultProps = {
+	title: 'Property Rentals',
+	description: 'Rental Properties for College Students',
+	keywords: 'Rentals, Property, Student',
+};
